760d6611b10e54d7f28e26d6c626fac8f494657e
Michelle Wiraydi <miwiryadi@gmail.com>
2017-12-29 11:01:58 +0000

    automatically backed up by learn

--- lib/play.rb
+++ lib/play.rb
@@ -1,3 +1,4 @@
+# Helper Methods
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -6,32 +7,28 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(user_input)
-   user_input.to_i - 1
- end
-
-def move(board, index, current_player = "X")
-   board[index] = current_player
+def move(board, location, current_player = "X")
+  board[location] = current_player
 end
 
 def position_taken?(board, location)
-   board[location] != " " && board[location] != ""
+  board[location] != " " && board[location] != ""
 end
 
-def valid_move?(board, index)
-   index.between?(0,8) && !position_taken?(board, index)
+def valid_move?(board, location)
+  location.between?(0,8) && !position_taken?(board, location)
 end
 
 def turn(board)
-   puts "Please enter 1-9:"
-   input = gets.strip
-   index = input_to_index(input)
-   if valid_move?(board, index)
-     move(board, index)
-     display_board(board)
-   else
-       turn(board)
-     end
+  puts "Please enter 1-9:"
+  input = gets.strip
+  location = input.to_i-1
+  if valid_move?(board, location)
+    move(board, location)
+    display_board(board)
+  else
+    turn(board)
+  end
 end
 
 # Define your play method below
