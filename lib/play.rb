9773b5fe02e89b9bcffa9bb7eb04bab58335f2c0
Michelle Wiraydi <miwiryadi@gmail.com>
2017-12-29 11:00:25 +0000

    automatically backed up by learn

--- lib/play.rb
+++ lib/play.rb
@@ -1,4 +1,3 @@
-# Helper Methods
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -8,31 +7,42 @@ def display_board(board)
 end
 
 def input_to_index(user_input)
-  user_input.to_i - 1
-end
+   user_input.to_i - 1
+ end
 
 def move(board, index, current_player = "X")
-  board[index] = current_player
+   board[index] = current_player
 end
 
 def position_taken?(board, location)
-  board[location] != " " && board[location] != ""
+   board[location] != " " && board[location] != ""
 end
 
 def valid_move?(board, index)
-  index.between?(0,8) && !position_taken?(board, index)
+   index.between?(0,8) && !position_taken?(board, index)
 end
 
 def turn(board)
-  puts "Please enter 1-9:"
-  input = gets.strip
-  index = input_to_index(input)
-  if valid_move?(board, index)
-    move(board, index)
-    display_board(board)
-  else
-    turn(board)
-  end
+   puts "Please enter 1-9:"
+   input = gets.strip
+   index = input_to_index(input)
+   if valid_move?(board, index)
+     move(board, index)
+     display_board(board)
+   else
+       turn(board)
+     end
 end
 
 # Define your play method below
+def play(board)
+  turns = 3
+  counter = 0
+  loop do
+    counter = counter + 1
+    turn(board)
+    if counter >= turns
+      puts "You only get #{turns} turns baby!"
+    end
+  end
+end
